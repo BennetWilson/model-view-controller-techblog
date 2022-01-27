bea14557e33e939eaeadfe60d2c3c6ca4ed81592
Bennet Wilson <bennetwilson@gmail.com>
2022-01-26 17:22:25 -0700

    finished my routes to the best of my ability

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const { Comment } = require('../../models/');
+const withAuth = requre('../../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({
+            include: [User],
+        });
+        const comments = commentData.map((comment) => comment.get({ plain: true}));
+
+        console.log(comments);
+        res.render('single-post', {comments, logged_in: req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body
+
+    try {
+        const newComment = await Comment.create({
+            ...body,
+            userId: req.session.userId,
+        });
+        res.json(newComment);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router
